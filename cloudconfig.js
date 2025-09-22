87e2ca1835874ce7ae0960c2276f75b6d41760fd
MansiGupta-444 <24guptam_1@rbunagpur.in>
2025-09-22 21:47:01 +0530

    Update cloudconfig.js

--- cloudconfig.js
+++ cloudconfig.js
@@ -1,6 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-const multer = require('multer');
-const { v4: uuid } = require('uuid');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -8,24 +7,16 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET
 });
 
-// Custom Cloudinary storage
-const storage = multer.memoryStorage(); // store file in memory first
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'library_DEV',
+    allowedFormat: ["png","jpg","jpeg"],
 
-const upload = multer({ storage });
+  },
+});
 
-async function uploadToCloudinary(file) {
-  const result = await cloudinary.uploader.upload_stream(
-    { folder: "library_DEV", public_id: uuid() },
-    (error, result) => {
-      if (error) throw error;
-      return result;
-    }
-  );
-  return result;
+module.exports={
+    cloudinary,
+    storage,
 }
-
-module.exports = {
-  cloudinary,
-  upload,
-  uploadToCloudinary
-};
